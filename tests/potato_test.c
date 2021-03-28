77339366020c582c51b7353a11c4b52fabc7ca13
Zhang Rui 张睿 <zrui16@hotmail.com>
2021-03-27 20:00:03 -0400

    Add tests

---
+++ tests/potato_test.c
@@ -0,0 +1,57 @@
+#include "potato.nb.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_TTL 500
+#define MAX_HOSTS 1000
+
+static void add_trace(Potato potato, uint16_t hostid)
+{
+	size_t len;
+	struct nbuf_obj o;
+
+	len = Potato_raw_trace(&o, potato);
+	if (len == 0) {
+		if (!Potato_alloc_trace(potato, 1))
+			return;
+	} else {
+		if (!nbuf_alloc(o.buf, sizeof hostid))
+			return;
+		nbuf_resize_arr(&o, len+1);
+	}
+	Potato_set_trace(potato, len, hostid);
+}
+
+static void print_trace(Potato potato)
+{
+	size_t i, n;
+
+	n = Potato_trace_size(potato);
+
+	printf("total hops: %u\n", (unsigned) n);
+	printf("trace:");
+	for (i = 0; i < n; i++) {
+		printf(" %u", Potato_trace(potato, i));
+	}
+	putchar('\n');
+}
+
+int main() {
+	struct nbuf buf[1];
+	Potato potato;
+
+	srand(time(NULL));
+	nbuf_init_rw(buf, 4096);
+	alloc_Potato(&potato, buf);
+	Potato_set_ttl(potato, rand() % MAX_TTL);
+
+	printf("expected hops: %u\n", (unsigned) Potato_ttl(potato));
+	while (Potato_ttl(potato) > 0) {
+		add_trace(potato, rand() % MAX_HOSTS);
+		Potato_set_ttl(potato, Potato_ttl(potato) - 1);
+	}
+	print_trace(potato);
+}
---
+++ tests/test.c
@@ -0,0 +1,85 @@
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif
+
+#include "test.nb.h"
+#include "libnbuf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <unistd.h>
+
+#define TOTAL_LOGS 100
+
+char *makemsg(int n)
+{
+	char *s;
+
+	if (asprintf(&s, "%d bottles of beer on the wall, %d bottles of beer\n"
+		"Take one down and pass it around, %d bottles of beer on the wall\n",
+		n, n, n-1) == -1)
+			return NULL;
+	return s;
+}
+
+int main()
+{
+	int i;
+	struct nbuf buf[1];
+	LogFile logfile;
+	logging_LogEntry entries;
+	const char output[] = "test.bin";
+
+#if 0
+	if (nbuf_load_file(buf, output)) {
+		struct nbuf_print_opt opt = {
+			.f = stdout,
+			.indent = 2,
+		};
+		get_LogFile(&logfile, buf, 0);
+		nbuf_print(&opt, NBUF_OBJ(logfile), refl_LogFile);
+		nbuf_unload_file(buf);
+		unlink(output);
+	}
+#endif
+	{
+		struct nbuf_print_opt opt = {
+			.f = stdout,
+			.indent = 2,
+		};
+		/* print self schema */
+		nbuf_print(&opt, NBUF_OBJ(refl_LogFile), nbuf_refl_MsgDef);
+	}
+	nbuf_init_rw(buf, 4096);
+	alloc_LogFile(&logfile, buf);
+	LogFile_alloc_log_entry(&entries, logfile, TOTAL_LOGS);
+
+	for (i = 0; i < TOTAL_LOGS; i++) {
+		logging_LogEntry entry;
+		datetime_Timestamp timestamp;
+		struct timespec ts;
+		char *msg;
+
+		LogFile_log_entry(&entry, logfile, i);
+		logging_LogEntry_alloc_timestamp(&timestamp, entry);
+		clock_gettime(CLOCK_REALTIME, &ts);
+		datetime_Timestamp_set_seconds(timestamp, ts.tv_sec);
+		datetime_Timestamp_set_nanoseconds(timestamp, ts.tv_nsec);
+		msg = makemsg(100-i);
+		logging_LogEntry_set_message(entry, msg, -1);
+		free(msg);
+		logging_LogEntry_set_severity(entry,
+			i > 98 ? logging_LogSeverity_CRITICAL :
+			i > 90 ? logging_LogSeverity_WARNING :
+			i > 60 ? logging_LogSeverity_NOTICE :
+			logging_LogSeverity_INFO);
+		usleep(1000);
+	}
+	fprintf(stderr, "dumping log to %s\n", output);
+	nbuf_save_file(buf, output);
+	nbuf_clear(buf);
+	return 0;
+}
